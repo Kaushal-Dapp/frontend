0feb92e061d1a1b0fb6f7b4f30d4ea157e57a30b
HARSHIT MISHRA <20bec017@iiitdwd.ac.in>
2023-05-14 11:12:08 +0530

    frontend added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <div className='absolute inset-x-0 text-center mx-auto bg-gray-800 text-gray-300 p-2 bottom-0 '>
+      <h1>Verify Your Skill(Your Virtual Resume)</h1>
+      <a
+        href='https://github.com/Adityaweb3/skill-verify'
+        src=''
+        className=' text-white text-xl hover:text-gray-300 absolute right-1 bottom-2 '>
+        <i className='fab fa-github'></i>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
